467b800d094be790ec7ee40ab2da6559865b976b
Rob Isenberg <robisenberg@me.com>
2015-01-18 22:11:17 +0000

    Include build/ directory

---
+++ js/plugins.js
@@ -0,0 +1,40 @@
+// Avoid `console` errors in browsers that lack a console.
+if (!(window.console && console.log)) {
+    (function() {
+        var noop = function() {};
+        var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
+        var length = methods.length;
+        var console = window.console = {};
+        while (length--) {
+            console[methods[length]] = noop;
+        }
+    }());
+}
+
+
+$(document).ready(function() {
+
+  var username = "naomi.grant87";
+  var hostname = "googlemail.com";
+  var linktext = username + "@" + hostname ;
+  $('.email').html("<a href='" + "mail" + "to:" + linktext + "@" + hostname + "'>" + linktext + "</a>");
+
+
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function(item) {
+        return item.el.attr('title') + '<small>by Naomi Grant</small>';
+      }
+    }
+  });
+});
